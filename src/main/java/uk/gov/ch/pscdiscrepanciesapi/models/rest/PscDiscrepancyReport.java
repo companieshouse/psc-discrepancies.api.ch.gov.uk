9ad4c9b274784bac73c874e0099e586ed1b21ee0
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-02-27 11:53:56 +0000

    Add mapstruct dependency to POM, create mapper for entityToRest and restToEntity, change access modifiers for models to be public to be accessed by other packages

--- src/main/java/uk/gov/ch/pscdiscrepanciesapi/models/rest/PscDiscrepancyReport.java
+++ src/main/java/uk/gov/ch/pscdiscrepanciesapi/models/rest/PscDiscrepancyReport.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
-class PscDiscrepancyReport {
+public class PscDiscrepancyReport {
 
     @JsonProperty("kind")
     private String kind;
---
+++ src/test/java/uk/gov/ch/pscdiscrepanciesapi/mapper/PscDiscrepancyReportMapperUnitTest.java
@@ -0,0 +1,83 @@
+package uk.gov.ch.pscdiscrepanciesapi.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.ch.pscdiscrepanciesapi.models.entity.PscDiscrepancyReportData;
+import uk.gov.ch.pscdiscrepanciesapi.models.entity.PscDiscrepancyReportEntity;
+import uk.gov.ch.pscdiscrepanciesapi.models.rest.PscDiscrepancyReport;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class PscDiscrepancyReportMapperUnitTest {
+
+    private PscDiscrepancyReportMapper pscDiscrepancyReportMapper;
+
+    private static final String KIND = "kind";
+    private static final String ETAG = "etag";
+    private static final String OBLIGED_ENTITY_NAME = "obligedEntityName";
+    private static final String OBLIGED_ENTITY_EMAIL = "obligedEntityEmail";
+    private static final String COMPANY_NUMBER = "companyNumber";
+    private static final Map<String, String> LINKS = new HashMap<>();
+
+    private PscDiscrepancyReport pscDiscrepancyReport;
+    private PscDiscrepancyReportEntity pscDiscrepancyReportEntity;
+    private PscDiscrepancyReportData pscDiscrepancyReportData;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        pscDiscrepancyReportMapper = Mappers.getMapper(PscDiscrepancyReportMapper.class);
+
+        pscDiscrepancyReportEntity = new PscDiscrepancyReportEntity();
+        pscDiscrepancyReportData = new PscDiscrepancyReportData();
+        pscDiscrepancyReportData.setKind(KIND);
+        pscDiscrepancyReportData.setEtag(ETAG);
+        pscDiscrepancyReportData.setObligedEntityName(OBLIGED_ENTITY_NAME);
+        pscDiscrepancyReportData.setObligedEntityEmail(OBLIGED_ENTITY_EMAIL);
+        pscDiscrepancyReportData.setCompanyNumber(COMPANY_NUMBER);
+        pscDiscrepancyReportData.setLinks(LINKS);
+        pscDiscrepancyReportEntity.setData(pscDiscrepancyReportData);
+
+        pscDiscrepancyReport = new PscDiscrepancyReport();
+        pscDiscrepancyReport.setKind(KIND);
+        pscDiscrepancyReport.setEtag(ETAG);
+        pscDiscrepancyReport.setObligedEntityName(OBLIGED_ENTITY_NAME);
+        pscDiscrepancyReport.setObligedEntityEmail(OBLIGED_ENTITY_EMAIL);
+        pscDiscrepancyReport.setCompanyNumber(COMPANY_NUMBER);
+        pscDiscrepancyReport.setLinks(LINKS);
+    }
+
+    @Test
+    @DisplayName("Test mapping of PscDiscrepancyReportEntity to PscDiscrepancyReport")
+    void validateEntityToRest() {
+        PscDiscrepancyReport result = pscDiscrepancyReportMapper.entityToRest(pscDiscrepancyReportEntity);
+
+        assertEquals(pscDiscrepancyReport.getKind(), result.getKind());
+        assertEquals(pscDiscrepancyReport.getEtag(), result.getEtag());
+        assertEquals(pscDiscrepancyReport.getObligedEntityName(), result.getObligedEntityName());
+        assertEquals(pscDiscrepancyReport.getObligedEntityEmail(), result.getObligedEntityEmail());
+        assertEquals(pscDiscrepancyReport.getCompanyNumber(), result.getCompanyNumber());
+        assertEquals(pscDiscrepancyReport.getLinks(), result.getLinks());
+    }
+
+    @Test
+    @DisplayName("Test mapping of PscDiscrepancyReport to PscDiscrepancyReportEntity")
+    void validateRestToEntity() {
+        PscDiscrepancyReportEntity result = pscDiscrepancyReportMapper.restToEntity(pscDiscrepancyReport);
+
+        assertEquals(pscDiscrepancyReportEntity.getData().getKind(), result.getData().getKind());
+        assertEquals(pscDiscrepancyReportEntity.getData().getEtag(), result.getData().getEtag());
+        assertEquals(pscDiscrepancyReportEntity.getData().getObligedEntityName(), result.getData().getObligedEntityName());
+        assertEquals(pscDiscrepancyReportEntity.getData().getObligedEntityEmail(), result.getData().getObligedEntityEmail());
+        assertEquals(pscDiscrepancyReportEntity.getData().getCompanyNumber(), result.getData().getCompanyNumber());
+        assertEquals(pscDiscrepancyReportEntity.getData().getLinks(), result.getData().getLinks());
+    }
+
+}
