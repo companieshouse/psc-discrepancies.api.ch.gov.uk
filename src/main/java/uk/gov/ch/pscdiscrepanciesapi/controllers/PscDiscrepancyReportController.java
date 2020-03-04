d260d0da696b29a70fcf85c36f5a1a283216252a
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-03-04 16:35:58 +0000

    Add working controller with GET by ID for report

---
+++ src/main/java/uk/gov/ch/pscdiscrepanciesapi/controllers/PscDiscrepancyReportController.java
@@ -0,0 +1,26 @@
+package uk.gov.ch.pscdiscrepanciesapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.ch.pscdiscrepanciesapi.models.rest.PscDiscrepancyReport;
+import uk.gov.ch.pscdiscrepanciesapi.services.PscDiscrepancyReportService;
+
+@RestController
+@RequestMapping("/psc-discrepancy-reports")
+public class PscDiscrepancyReportController {
+
+    @Autowired
+    private PscDiscrepancyReportService pscDiscrepancyReportService;
+
+    @GetMapping("/{psc-discrepancy-report-id}")
+    public ResponseEntity get(@PathVariable("psc-discrepancy-report-id") String id) {
+        PscDiscrepancyReport pscDiscrepancyReport =
+                pscDiscrepancyReportService.findPscDiscrepancyReportById(id);
+
+        return ResponseEntity.ok(pscDiscrepancyReport);
+    }
+}
