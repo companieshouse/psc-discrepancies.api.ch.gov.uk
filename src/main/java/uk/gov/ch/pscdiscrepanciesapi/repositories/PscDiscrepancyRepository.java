eeff88fe5f6eae25cdb7d451dd2317cf44548e72
Jonathan Blake <jblake2@companieshouse.gov.uk>
2021-05-12 08:28:09 +0100

    Made Mongo query dependent on parameterised sting

--- src/main/java/uk/gov/ch/pscdiscrepanciesapi/repositories/PscDiscrepancyRepository.java
+++ src/main/java/uk/gov/ch/pscdiscrepanciesapi/repositories/PscDiscrepancyRepository.java
@@ -1,16 +1,15 @@
 package uk.gov.ch.pscdiscrepanciesapi.repositories;
 
 import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import uk.gov.ch.pscdiscrepanciesapi.common.PscDiscrepancyLinkKeys;
 import uk.gov.ch.pscdiscrepanciesapi.models.entity.PscDiscrepancyEntity;
 
 @Repository
 public interface PscDiscrepancyRepository extends MongoRepository<PscDiscrepancyEntity, String> {
 	
-	@Query(value="{'data.links.psc-discrepancy-report' : ?0}")
-	public List<PscDiscrepancyEntity> getDiscrepancies(String reportLink);
+	@Query(value="{'data.links."+ PscDiscrepancyLinkKeys.REPORT_LINK+"' : ?0}")
+	List<PscDiscrepancyEntity> getDiscrepancies(String reportLink);
 }
